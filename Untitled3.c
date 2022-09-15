ae4bd33795b380c3327e1162c1cb9f1dbfb3a38c
Shreya <shreyadusane95@gmail.com>
2022-09-15 19:04:53 +0530

    project1

---
+++ Untitled3.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+float area(int length,int breadth){
+
+ return(length*breadth);
+
+
+}
+
+
+int main(){
+  float length,breadth,area;
+  int i;
+  int b;
+  int a[10]={15,30,45,63,72,80,86,97,56,25};
+  for(i=0;i<=10;i++){
+
+   a[10]=length;
+
+
+  }
+   printf("Enter the value of length:");
+ scanf("%f",&length);
+
+ printf("Enter the value of breadth: ");
+ scanf("%f",&breadth);
+
+area=(length*breadth);
+
+  printf("Area of Rectangle : %f\n",area);
+
+return 0;
+
+
+
+
+}
+
