0a7e6d9ff51187fb20cb2d76f86098010396899f
Shreya <shreyadusane95@gmail.com>
2022-09-16 18:15:57 +0530

    project 1new

--- Untitled3.c
+++ Untitled3.c
@@ -1,37 +1,56 @@
-#include<stdio.h>
+#include <stdio.h>
 
-float area(int length,int breadth){
-
- return(length*breadth);
-
+float calc_area(int l, int w){
+  return (l * w);
 }
 
-
-int main(){
-  float length,breadth,area;
+int main()
+{
   int i;
   int b;
-  int a[10]={15,30,45,63,72,80,86,97,56,25};
-  for(i=0;i<=10;i++){
+  int a[10] = {15, 30, 45, 63, 72, 80, 86, 97, 56, 25};
+  int areaList[10] = {0};
+  int length, breadth ;
+  float area;
+  for (i = 0; i <= 10; i++)
+  {
+    length = a[i];
+    printf("Rectangle Name R%d ", (i+1));
+    printf("Value of length: %d ", length);
 
-   a[10]=length;
-
-
+    printf("Enter the value of breadth: ");
+    scanf("%d", &breadth);
+    area = calc_area(length, breadth)
+    areaList[i] =area;
+    printf("Area is : %f ", area);
   }
-   printf("Enter the value of length:");
- scanf("%f",&length);
-
- printf("Enter the value of breadth: ");
- scanf("%f",&breadth);
-
-area=(length*breadth);
-
-  printf("Area of Rectangle : %f\n",area);
-
-return 0;
-
-
-
-
+ 
+  return 0;
 }
 
+
+
+
+// // definition of function
+// sin(angle){
+//   if (angle==45) return (1/1.421)
+//   if (angle==60) return (1/2)
+// }
+
+// chaha(int goad){
+//   //
+//   return 20;
+// }
+
+
+// //
+// int x = 45
+// int y = 60
+
+// sin(x);
+// sin(angle);
+
+
+// int material = "sakhar"
+// chaha(goad)
+// chaha(goad)
